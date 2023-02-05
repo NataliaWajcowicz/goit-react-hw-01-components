c9999fbbfc60cb7440d761e35cb2187d0f33dfc8
NataliaWajcowicz <n.wajcowicz@o2.pl>
2023-02-05 17:46:44 +0100

    friends css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import friends from './friends.json';
 import { FriendListItem } from './FriendListItem';
+import css from './style.module.css';
 
 const FriendList = props => (
-    <ul>
+    <ul className={css.friendsList}>
         {friends.map(friend => (
             <FriendListItem key={friend.id} friends={friend} />
         ))}
+
         
     </ul>
     
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,15 @@
+import css from './style.module.css';
 
-const FriendListItem = ({ friends }) => (
-    <li>
-        <span>{friends.isOnline }</span>
-        <img src={friends.avatar} alt="User avatar" width="48" />
-        <p>{ friends.name}</p>
-    </li>
-);
+const FriendListItem = ({ friends }) => {
+    return(
+        <li className={css.friendsListItem}>
+            {friends.isOnline === true ? (
+                <span className={css.online}></span>)
+            :(<span className={css.offline}></span>)}
+            <img className={css.friendPicture} src={friends.avatar} alt="User avatar" width="48" />
+            <p className={css.friendName}>{friends.name}</p>
+        </li>)
+    
+};
 
 export { FriendListItem };
